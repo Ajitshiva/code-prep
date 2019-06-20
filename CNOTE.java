c2233c8f916ea4150ba8668b1d4b940f53374bc3
Ajitshiva <ajitshiva007@gmail.com>
2019-06-20 17:51:33 +0530

    Chef and Notebooks [CODECHEF]

---
+++ CNOTE.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class CNOTE
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0)
+        {
+            int Total = sc.nextInt();
+            int Done = sc.nextInt();
+            int Rubels = sc.nextInt();
+            int N = sc.nextInt();
+            int Pages[] = new int[N];
+            int Prices[] = new int[N];
+            int flag=0;
+            for(int i=0;i<N;i++)
+            {
+                Pages[i] =sc.nextInt();
+                Prices[i] = sc.nextInt(); 
+            }
+            for(int i=0;i<N;i++)
+            {
+                if(Rubels>=Prices[i] && (Total-Done)<=Pages[i])
+                    flag=1;
+            }
+            if(flag==1)
+                System.out.println("LUCKYCHEF");
+            else
+                System.out.println("UNLUCKYCHEF");
+        }
+    }
+}
