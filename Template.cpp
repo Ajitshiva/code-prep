17ec0f66d99a02f842e42fb102d91e8018a41d6b
Ajit Shiva <ajitshiva007@gmail.com>
2020-07-22 17:32:10 +0530

    Template added

--- Template.cpp
+++ Template.cpp
@@ -1,120 +1,94 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <numeric>
+#include <bitset>
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>*/
+
+#define sz(x) (int)(x).size()
+
+
+// #pragma GCC optimize("O3")
 using namespace std;
-typedef long long int ll;
-const int INF = 0x3f3f3f3f;
-const double pi = 3.14;
-const ll mod = 1000000007;
-ll gcd(ll a, ll b)
-{
-	if(b==0) return a;
+// using namespace __gnu_pbds;
 
-	else return gcd(b, a%b);
-}
-ll lcm(ll a, ll b)
-{
-	return (a*b)/gcd(a,b);
-}
-ll fastpower(ll base, ll exp, ll mod)
-{
-	ll ans = 1ll;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
 
-	while(exp)
-	{
-		if(exp&1)
-		{
-			ans = (ans%mod * base%mod)%mod;
-		}
-		exp/=2;
-		base = (base%mod * base%mod)%mod;
-	}
-	return ans;
-}
-const int MAX = 1e5+9;
-bool visited[MAX];
-vector< int > adj[MAX+1];
-void dfs(int node)
-{
-	visited[node] = true;
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";} void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
-	for(auto u : adj[node])
-	{
-		if(!visited[u])
-		{
-			dfs(u);
-		}
-	}
-}
-void replaceElements(ll arr[], ll n) 
-{ 
-    unordered_set<ll> s; 
-  
-    for (int i = 1; i <= n; i++) { 
-  
-        
-        if (s.find(arr[i]) == s.end()) 
-            s.insert(arr[i]); 
-  
-        else { 
-  
-            
-            for (ll j = arr[i] + 1ll; j < LLONG_MAX; j++) { 
-                if (s.find(j) == s.end()) { 
-                    arr[i] = j; 
-                    s.insert(j); 
-                    break; 
-                } 
-            } 
-        } 
-    } 
-} 
-int main() {
-	// your code goes here
-	
-	freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-	int t; cin >> t;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// === Debug macro ends here ===q
+ 
+#define ll   long long
+#define ld   long double
+ 
+#define mp   make_pair
+#define pb   push_back
+#define eb   emplace_back
+ 
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+/*template <typename T> 
+using ordered_set = tree< T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;*/
+template<typename T>             istream& operator>>(istream& is,  vector<T>& v){for (auto& i : v) is >> i;        return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>& v){for (auto& i : v) os << i << ' '; return os;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U>& p){is >> p.first >> p.second;        return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>& p){os << p.first << ' ' << p.second; return os;}
+template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){return mp(a.first-b.first, a.second-b.second);}
+template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
+template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
+
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif 
+	int t;
+	cin >> t;
 	while(t--)
 	{
 
-		ll n, k;
-		cin >> n >> k;
-		ll arr[n+1];
-		ll pos[n+1];
-		ll temp[n+1];
-		// unordered_map<, int>cnt;
-		int l = 0;
-		for(int i = 1 ; i <= n ; i++)
-		{
-			cin >> arr[i]; 
-		}
-		replaceElements(arr,n+1);
-		for(int i = 1 ; i <= n ; i++)
-		{
-			//cout << arr[i] << endl;
-			temp[i] = arr[i];
-		}
-		sort(temp+1,temp+n+1);
-		unordered_map <ll, ll> mp;
-
-		for(int i = 1; i <=n ; i++)
-		{
-			mp[temp[i]] = i; 
-			//cout << temp[i] << " " << i << endl;
-		}
-
-		for(int i = 1; i <=n ; i++)
-		{
-			pos[i] = mp[arr[i]];
-		}
-		bool flag = true;
-		for(int i = 1 ; i <=n ; i++)
-		{
-			if(abs(i-pos[i])%k != 0) flag = false; 
-			//cout << i << " " << pos[i] << '\n'; 
-		}
-
-		if(flag) cout << "yes" << '\n';
-		else cout << "no" << '\n';
 
 	}
 	return 0;
-	} 
+}
+
+
+
+
